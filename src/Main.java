1a59b2b7f2e06d3c63c3cfb378572a402b5203da
ImamM5 <imamm@nycstudents.net>
2022-11-17 09:29:31 -0500

    new code

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args)
+    {
+        for (int row = 1; row <= 9; row++)
+        {
+            int x = 0;
+            for (int col = 1; col <= 3; col++)
+            {
+                System.out.print(x + " "+ col);
+            }
+            x++;
+            System.out.println();
+        }
+
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Test {
+public class Test
+{
+    public Test()
+    {
+
+    }
+    public void printNums (int value, int numRounds)
+    {
+
+        for (int i =1; i <= numRounds; i++)
+        {
+            int x = -1;
+            while (x != value)
+            {
+                x = (int)(Math.random()*10);
+                System.out.print(x);
+            }
+            System.out.println();
+        }
+
+
+    }
 }
