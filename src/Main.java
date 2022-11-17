e69e2e65151ac0b10864b8382cf6bbedc94e01e6
Imam Mishkat <imammishkat@gmail.com>
2022-11-17 17:08:54 -0500

    new code

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,23 @@
 public class Main {
     public static void main(String[] args)
     {
-        for (int row = 1; row <= 9; row++)
-        {
-            int x = 0;
-            for (int col = 1; col <= 3; col++)
-            {
-                System.out.print(x + " "+ col);
+        int result = 0;
+        int m = 0;
+        while (m < 3) {
+            int k = 0;
+            while (k < 4) {
+                result++;
+                k++;
+                System.out.print("k: " + k);
+                System.out.println(", m: " + m);
             }
-            x++;
-            System.out.println();
+            m++;
         }
+        System.out.println("result: " + result);
+
+
+
+
 
     }
 }
